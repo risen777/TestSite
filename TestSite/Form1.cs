aa6a10a692e24307158a47548eb72b8c262de3db
risen777 <21risen@gmail.com>
2016-12-15 23:06:21 +0300

    тест пример 1

--- TestSite/Form1.cs
+++ TestSite/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using OpenQA.Selenium;
 namespace TestSite
 {
     public partial class Form1 : Form
     {
+        IWebDriver Browser;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Manage().Window.Maximize();
+            Browser.Navigate().GoToUrl("http://google.com");
+
+            IWebElement SearchInput = Browser.FindElement(By.Id("lst-ib"));
+            SearchInput.SendKeys("летающиqе лошади" + OpenQA.Selenium.Keys.Enter);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Browser.Quit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
