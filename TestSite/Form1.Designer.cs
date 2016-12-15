641d61995ee738193cbde6c751bdb0ec34ff3a5f
risen777 <21risen@gmail.com>
2016-12-16 01:55:36 +0300

    Новые тесты

--- TestSite/Form1.Designer.cs
+++ TestSite/Form1.Designer.cs
@@ -30,36 +30,73 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(404, 66);
+            this.button1.Location = new System.Drawing.Point(51, 51);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "1 Пример";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(427, 157);
+            this.button2.Location = new System.Drawing.Point(60, 143);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Выход";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(269, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(205, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "2 Поиск элементов";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(280, 130);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Открыть браузер";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(313, 190);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(167, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Выполнить JS";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(505, 261);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
 
@@ -69,6 +106,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- TestSite/Form1.cs
+++ TestSite/Form1.cs
@@ -21,8 +21,7 @@ namespace TestSite
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
-            Browser.Manage().Window.Maximize();
+            
             Browser.Navigate().GoToUrl("http://google.com");
 
             IWebElement SearchInput = Browser.FindElement(By.Id("lst-ib"));
@@ -37,6 +36,28 @@ namespace TestSite
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IWebElement element;
+            //  IWebElement element = Browser.FindElement(By.Id("text"));
+            //  element.SendKeys("тест");
+
+            element = Browser.FindElement(By.ClassName("footbal_close"));
+            element.Click();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Manage().Window.Maximize();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            IJavaScriptExecutor JSE = Browser as IJavaScriptExecutor;
+            JSE.ExecuteScript("alert('всплывающее сообщение');");
         }
     }
 }
